bbc873288e3223d77ebddec88875cd215a458449
AndreyAgeev <36598717+AndreyAgeev@users.noreply.github.com>
2018-04-03 19:32:11 +0300

    Update comment

--- [DM]1/Header.h
+++ [DM]1/Header.h
@@ -1,5 +1,5 @@
 #pragma once
-//#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -27,9 +27,9 @@ void Begin(void);
 void CheckError(int *, int, int);
 int CheckInt(unsigned int , unsigned int );
 void H(void);
-int U(unsigned int , unsigned int , int *);// число размещений U(m,n) = m^n
-int A(unsigned int , unsigned int , int *);// число размещений без повторений  A(m,n) = m!/(m-m)! = m(m-1)....(m - n + 1) 
-int P(unsigned int , int * );// число перестановок P(n) = n!
-int S(unsigned int , unsigned int , int * );// число Стирлинга второго рода S(m,n) = S(m-1, n-1) + n * S(m-1, n) //НЕ РОБИТ
-int C(unsigned int, unsigned  int, int *);// число сочетаний C(m,n) = m!(n!*(m-n)!) = A(m,n)/ P(n)
-int B(unsigned int , int * );// число белла B(m) = Zi=0, m S(m,n)
+int U(unsigned int , unsigned int , int *);//  U(m,n) = m^n
+int A(unsigned int , unsigned int , int *);//  A(m,n) = m!/(m-m)! = m(m-1)....(m - n + 1) 
+int P(unsigned int , int * );// P(n) = n!
+int S(unsigned int , unsigned int , int * );// S(m,n) = S(m-1, n-1) + n * S(m-1, n)
+int C(unsigned int, unsigned  int, int *);// C(m,n) = m!(n!*(m-n)!) = A(m,n)/ P(n)
+int B(unsigned int , int * );// B(m) = Zi=0, m S(m,n)
